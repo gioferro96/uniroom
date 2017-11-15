c5414eea3dd0359e133153470edcc2013e57c6b4
gioferro96 <giovanni.ferronato@studenti.unitn.it>
2017-11-15 09:14:21 +0100

    footer problem resolved

--- client/js/myjs.js
+++ client/js/myjs.js
@@ -26,9 +26,21 @@ function getPlace(){
 
 function enter() {
 	document.getElementById("inserisci").addEventListener("keyup", function(event) {
-	event.preventDefault();
-	if (event.keyCode === 13) {
-		document.getElementById("icon").click();
-	}
-});
+		event.preventDefault();
+		if (event.keyCode === 13) {
+			document.getElementById("icon").click();
+		}
+	});
+}
+
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return pair[1];
+		}
+	} 
+	alert('Query Variable ' + variable + ' not found');
 }
